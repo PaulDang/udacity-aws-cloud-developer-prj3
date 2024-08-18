05790e46d1f2822590979a863482431e26686359
Paul Dang <dangvanthanhdli@gmail.com>
2024-08-19 01:12:20 +0700

    code project 3 udacity

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  username: "johnny",
+  password: "mypassword",
+  database: "prj3",
+  host: "prj3.ckoagyf3yjke.us-east-1.rds.amazonaws.com",
+  dialect: "postgres",
+  aws_region: "us-east-1",
+  aws_profile: "default",
+  aws_media_bucket: "arn:aws:s3:::mybucket1200798",
+  url: "http://localhost:8100",
+  jwt: {
+    secret: "testing",
+  },
+};
